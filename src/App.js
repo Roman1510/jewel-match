43faa691099f2c274b4fc0432f827b5a014c6ff6
Roman Vinnick <vinnick@Romans-MacBook-Air.local>
2021-10-23 21:24:10 +0200

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const width = 8
 const candyColors = [
@@ -12,6 +12,18 @@ const candyColors = [
 
 const  App = ()=> {
     const [currColorArr, setCurrColorArr] = useState([])
+
+    const checkForCol = () =>{
+        for (let i=0; i<47; i++){
+            const colOfThree = [i, i+width,i+width*2]
+            const decidedColor = currColorArr[i]
+
+            if(colOfThree.every(square=>currColorArr[square] === decidedColor)){
+                colOfThree.forEach(square => currColorArr[square] = '')
+            }
+        }
+    }
+
     const createBoard = () =>{
         const colorsArray = []
         for(let i = 0; i<width*width;i++){
@@ -19,13 +31,34 @@ const  App = ()=> {
             const randomColor = candyColors[Math.floor(randomNumber)]
             colorsArray.push(randomColor)
         }
-
+        setCurrColorArr(colorsArray)
     }
 
-    createBoard()
+    useEffect(()=>{
+        createBoard()
+    }, [])
 
+    useEffect(()=>{
+       const timer = setInterval(()=>{
+           checkForCol()
+           setCurrColorArr([...currColorArr])
+       }, 100)
+        return () =>clearInterval(timer)
+    },[checkForCol])
+
+    console.log(currColorArr)
   return (
-    <div></div>
+    <div className="app">
+        <div className="game">
+            {currColorArr.map((color, index)=>(
+                <img
+                    key={index}
+                    style={{backgroundColor: color}}
+                    alt={color}
+                />
+            ))}
+        </div>
+    </div>
   );
 }
 
