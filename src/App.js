11326acf30fb2e9802acd1578a9ee2f0dd0d8b17
Roman Vinnick <vinnick@Romans-MacBook-Air.local>
2021-10-24 20:26:00 +0200

    adding drag n drop logic

--- src/App.js
+++ src/App.js
@@ -10,56 +10,136 @@ const candyColors = [
     'yellow'
 ]
 
-const  App = ()=> {
+const App = () => {
     const [currColorArr, setCurrColorArr] = useState([])
+    const [beingDragged,setBeingDragged] = useState(null)
+    const [beingReplaced,setBeingReplaced] = useState(null)
 
-    const checkForCol = () =>{
-        for (let i=0; i<47; i++){
-            const colOfThree = [i, i+width,i+width*2]
+    const checkForCol3 = () => {
+        for (let i = 0; i <= 47; i++) {
+            const colOf3 = [i, i + width, i + width * 2]
             const decidedColor = currColorArr[i]
 
-            if(colOfThree.every(square=>currColorArr[square] === decidedColor)){
-                colOfThree.forEach(square => currColorArr[square] = '')
+            if (colOf3.every(square => currColorArr[square] === decidedColor)) {
+                colOf3.forEach(square => currColorArr[square] = '')
+            }
+        }
+    }
+    const checkForCol4 = () => {
+        for (let i = 0; i <= 39; i++) {
+            const colOf4 = [i, i + width, i + width * 2, i + width * 3]
+            const decidedColor = currColorArr[i]
+
+            if (colOf4.every(square => currColorArr[square] === decidedColor)) {
+                colOf4.forEach(square => currColorArr[square] = '')
             }
         }
     }
 
-    const createBoard = () =>{
+    const checkForRow3 = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOf3 = [i, i + 1, i + 2]
+            const decidedColor = currColorArr[i]
+            const endList = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+            if (endList.includes(i)) continue
+            if (rowOf3.every(square => currColorArr[square] === decidedColor)) {
+                rowOf3.forEach(square => currColorArr[square] = '')
+            }
+        }
+    }
+    const checkForRow4 = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOf4 = [i, i + 1, i + 2, i + 3]
+            const decidedColor = currColorArr[i]
+            const endList = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+            if (endList.includes(i)) continue
+            if (rowOf4.every(square => currColorArr[square] === decidedColor)) {
+                rowOf4.forEach(square => currColorArr[square] = '')
+            }
+        }
+    }
+
+
+    const moveIntoSquareBelow = () => {
+        for (let i = 0; i <= 64 - width; i++) {
+
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+            const isFirstRow = firstRow.includes(i)
+            if (isFirstRow && currColorArr[i] === '') {
+                let randColor = Math.floor(Math.random() * candyColors.length)
+                currColorArr[i] = candyColors[randColor]
+            }
+
+            if ((currColorArr[i + width]) === '') {
+                currColorArr[i + width] = currColorArr[i]
+                currColorArr[i] = ''
+            }
+        }
+    }
+
+    const dragStart = (e) => {
+        setBeingDragged(e.target)
+    }
+
+    const dragDrop = (e) => {
+        setBeingReplaced(e.target)
+    }
+
+    const dragEnd = (e) => {
+        const beingReplacedId = beingReplaced.getAttribute('data-id')
+        const beingDraggedId = beingDragged.getAttribute('data-id')
+
+        console.log(beingReplacedId,beingDraggedId)
+    }
+
+    const createBoard = () => {
         const colorsArray = []
-        for(let i = 0; i<width*width;i++){
-            const randomNumber = Math.random()*candyColors.length
+        for (let i = 0; i < width * width; i++) {
+            const randomNumber = Math.random() * candyColors.length
             const randomColor = candyColors[Math.floor(randomNumber)]
             colorsArray.push(randomColor)
         }
         setCurrColorArr(colorsArray)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         createBoard()
     }, [])
 
-    useEffect(()=>{
-       const timer = setInterval(()=>{
-           checkForCol()
-           setCurrColorArr([...currColorArr])
-       }, 100)
-        return () =>clearInterval(timer)
-    },[checkForCol])
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForCol4()
+            checkForCol3()
+            checkForRow4()
+            checkForRow3()
+            moveIntoSquareBelow()
+            setCurrColorArr([...currColorArr])
+        }, 100)
+        return () => clearInterval(timer)
+    }, [checkForCol4, checkForCol3, checkForRow4, checkForRow3, moveIntoSquareBelow, currColorArr])
 
-    console.log(currColorArr)
-  return (
-    <div className="app">
-        <div className="game">
-            {currColorArr.map((color, index)=>(
-                <img
-                    key={index}
-                    style={{backgroundColor: color}}
-                    alt={color}
-                />
-            ))}
+
+    return (
+        <div className="app">
+            <div className="game">
+                {currColorArr.map((color, index) => (
+                    <img
+                        key={index}
+                        style={{backgroundColor: color}}
+                        alt={color}
+                        data-id={index}
+                        draggable={true}
+                        onDragStart={dragStart}
+                        onDragOver={(e) => e.preventDefault()}
+                        onDragEnter={(e) => e.preventDefault()}
+                        onDragLeave={(e) => e.preventDefault()}
+                        onDrop={dragDrop}
+                        onDragEnd={dragEnd}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
