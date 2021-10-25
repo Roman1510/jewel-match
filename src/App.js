a4e0d8984845c905eeb5b3a8e709369a57ac1966
Vinnick <romanvinnick@gmail.com>
2021-10-25 21:44:52 +0200

    score fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import {useState, useEffect} from "react";
+import Scores from "./components/Scores";
+import blueJewel from './images/blue.png'
+import greenJewel from './images/green.png'
+import redJewel from './images/red.png'
+import orangeJewel from './images/orange.png'
+import yellowJewel from './images/yellow.png'
+import purpleJewel from './images/purple.png'
+import blankJewel from './images/blank.png'
+
 
 const width = 8
-const candyColors = [
-    'blue',
-    'green',
-    'orange',
-    'purple',
-    'red',
-    'yellow'
+const jewelColors = [
+    blueJewel,
+    greenJewel,
+    orangeJewel,
+    purpleJewel,
+    redJewel,
+    yellowJewel
 ]
 
 const App = () => {
     const [currColorArr, setCurrColorArr] = useState([])
     const [beingDragged, setBeingDragged] = useState(null)
     const [beingReplaced, setBeingReplaced] = useState(null)
+    const [currentScore, setCurrentScore] = useState(0)
+    const [alreadyDragged, setAlreadyDragged] = useState(false)
+
 
     const checkForCol3 = () => {
         for (let i = 0; i <= 47; i++) {
             const colOf3 = [i, i + width, i + width * 2]
             const decidedColor = currColorArr[i]
-
-            if (colOf3.every(square => currColorArr[square] === decidedColor)) {
-                colOf3.forEach(square => currColorArr[square] = '')
+            const isBlank = currColorArr[i] === blankJewel
+            if (colOf3.every(square => currColorArr[square] === decidedColor && !isBlank)) {
+                if (alreadyDragged) {
+                    setCurrentScore((score) => score + 3)
+                }
+                colOf3.forEach(square => currColorArr[square] = blankJewel)
                 return true
             }
         }
@@ -30,22 +45,30 @@ const App = () => {
         for (let i = 0; i <= 39; i++) {
             const colOf4 = [i, i + width, i + width * 2, i + width * 3]
             const decidedColor = currColorArr[i]
-
-            if (colOf4.every(square => currColorArr[square] === decidedColor)) {
-                colOf4.forEach(square => currColorArr[square] = '')
+            const isBlank = currColorArr[i] === blankJewel
+            if (colOf4.every(square => currColorArr[square] === decidedColor && !isBlank)) {
+                if (alreadyDragged) {
+                    setCurrentScore((score) => score + 4)
+                }
+                colOf4.forEach(square => currColorArr[square] = blankJewel)
                 return true
             }
         }
     }
 
     const checkForRow3 = () => {
+        console.log(alreadyDragged)
         for (let i = 0; i < 64; i++) {
             const rowOf3 = [i, i + 1, i + 2]
             const decidedColor = currColorArr[i]
+            const isBlank = currColorArr[i] === blankJewel
             const endList = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
             if (endList.includes(i)) continue
-            if (rowOf3.every(square => currColorArr[square] === decidedColor)) {
-                rowOf3.forEach(square => currColorArr[square] = '')
+            if (rowOf3.every(square => currColorArr[square] === decidedColor && !isBlank)) {
+                if(alreadyDragged){
+                    setCurrentScore((score) => score + 3)
+                }
+                rowOf3.forEach(square => currColorArr[square] = blankJewel)
                 return true
             }
         }
@@ -54,10 +77,14 @@ const App = () => {
         for (let i = 0; i < 64; i++) {
             const rowOf4 = [i, i + 1, i + 2, i + 3]
             const decidedColor = currColorArr[i]
+            const isBlank = currColorArr[i] === blankJewel
             const endList = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
             if (endList.includes(i)) continue
-            if (rowOf4.every(square => currColorArr[square] === decidedColor)) {
-                rowOf4.forEach(square => currColorArr[square] = '')
+            if (rowOf4.every(square => currColorArr[square] === decidedColor && !isBlank)) {
+                if(alreadyDragged){
+                    setCurrentScore((score) => score + 4)
+                }
+                rowOf4.forEach(square => currColorArr[square] = blankJewel)
                 return true
             }
         }
@@ -69,19 +96,20 @@ const App = () => {
 
             const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
             const isFirstRow = firstRow.includes(i)
-            if (isFirstRow && currColorArr[i] === '') {
-                let randColor = Math.floor(Math.random() * candyColors.length)
-                currColorArr[i] = candyColors[randColor]
+            if (isFirstRow && currColorArr[i] === blankJewel) {
+                let randColor = Math.floor(Math.random() * jewelColors.length)
+                currColorArr[i] = jewelColors[randColor]
             }
 
-            if ((currColorArr[i + width]) === '') {
+            if ((currColorArr[i + width]) === blankJewel) {
                 currColorArr[i + width] = currColorArr[i]
-                currColorArr[i] = ''
+                currColorArr[i] = blankJewel
             }
         }
     }
 
     const dragStart = (e) => {
+        setAlreadyDragged(true)
         setBeingDragged(e.target)
     }
 
@@ -89,13 +117,13 @@ const App = () => {
         setBeingReplaced(e.target)
     }
 
-    const dragEnd = (e) => {
+    const dragEnd = () => {
+
         const beingReplacedId = parseInt(beingReplaced.getAttribute('data-id'))
         const beingDraggedId = parseInt(beingDragged.getAttribute('data-id'))
 
-        console.log(currColorArr[beingReplacedId], beingDraggedId)
-        currColorArr[beingReplacedId] = beingDragged.style.backgroundColor
-        currColorArr[beingDraggedId] = beingReplaced.style.backgroundColor
+        currColorArr[beingReplacedId] = beingDragged.getAttribute('src')
+        currColorArr[beingDraggedId] = beingReplaced.getAttribute('src')
 
         const validMoves = [
             beingDraggedId - 1,
@@ -114,22 +142,31 @@ const App = () => {
             setBeingDragged(null)
             setBeingReplaced(null)
         } else {
-            currColorArr[beingReplacedId] = beingReplaced.style.backgroundColor
-            currColorArr[beingDraggedId] = beingDragged.style.backgroundColor
+            currColorArr[beingReplacedId] = beingReplaced.getAttribute('src')
+            currColorArr[beingDraggedId] = beingDragged.getAttribute('src')
             setCurrColorArr([...currColorArr])
         }
 
+        gameLogic()
 
     }
 
     const createBoard = () => {
         const colorsArray = []
         for (let i = 0; i < width * width; i++) {
-            const randomNumber = Math.random() * candyColors.length
-            const randomColor = candyColors[Math.floor(randomNumber)]
+            const randomNumber = Math.random() * jewelColors.length
+            const randomColor = jewelColors[Math.floor(randomNumber)]
             colorsArray.push(randomColor)
         }
         setCurrColorArr(colorsArray)
+    }
+
+    const gameLogic = () => {
+        checkForCol4()
+        checkForCol3()
+        checkForRow4()
+        checkForRow3()
+        moveIntoSquareBelow()
     }
 
     useEffect(() => {
@@ -138,11 +175,7 @@ const App = () => {
 
     useEffect(() => {
         const timer = setInterval(() => {
-            checkForCol4()
-            checkForCol3()
-            checkForRow4()
-            checkForRow3()
-            moveIntoSquareBelow()
+            gameLogic()
             setCurrColorArr([...currColorArr])
         }, 100)
         return () => clearInterval(timer)
@@ -153,23 +186,31 @@ const App = () => {
         <div className="app">
             <div className="game">
                 {currColorArr.map((color, index) => (
-                    <img
-                        key={index}
-                        style={{backgroundColor: color}}
-                        alt={color}
-                        data-id={index}
-                        draggable={true}
-                        onDragStart={dragStart}
-                        onDragOver={(e) => e.preventDefault()}
-                        onDragEnter={(e) => e.preventDefault()}
-                        onDragLeave={(e) => e.preventDefault()}
-                        onDrop={dragDrop}
-                        onDragEnd={dragEnd}
-                    />
+                    <div className="grid">
+                        <img
+                            key={index}
+                            src={color}
+                            alt={color}
+                            data-id={index}
+                            draggable={true}
+                            onDragStart={dragStart}
+                            onDragOver={(e) => e.preventDefault()}
+                            onDragEnter={(e) => e.preventDefault()}
+                            onDragLeave={(e) => e.preventDefault()}
+                            onDrop={dragDrop}
+                            onDragEnd={dragEnd}
+                        />
+                    </div>
+
                 ))}
             </div>
+            <Scores score={currentScore}/>
         </div>
     );
 }
 
 export default App;
+// 1. Get rid of repetitiveness
+// 2. Make score count only after it's fully static
+// 3. Make background
+// 4. Center the whole game
---
+++ src/components/Scores.js
@@ -0,0 +1,9 @@
+const Scores = ({score})=>{
+    return (
+        <div className="score-board">
+            <h2>{score}</h2>
+        </div>
+    )
+}
+
+export default Scores
